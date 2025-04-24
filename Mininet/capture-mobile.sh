f78443d7242fbea54e424765c568c1d5a0ba9915
Salman Alfarizi <100526318+MELON-27AF@users.noreply.github.com>
2025-04-24 13:38:55 +0700

    Delete Mininet directory

--- Mininet/capture-mobile.sh
+++
@@ -1,3 +0,0 @@
-sh tshark -i ap1-wlan1 -w /root/hasil_test/ap1-fixed -F pcapng -a duration:180 &
-sh tshark -i ap2-wlan1 -w /root/hasil_test/ap2-fixed -F pcapng -a duration:180 &
-sh tshark -i ap3-wlan1 -w /root/hasil_test/ap3-fixed -F pcapng -a duration:180 &
