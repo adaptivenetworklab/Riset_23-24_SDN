54433e6651119310fc38c2911854f798ad3e910e
Salman Alfarizi <100526318+MELON-27AF@users.noreply.github.com>
2025-06-11 10:50:35 +0700

    Delete Open5Gs-UERANSIM directory

--- Open5Gs-UERANSIM/images/Open5Gs/image_info.sh
+++
@@ -1,3 +0,0 @@
-IMAGE_TAG=2.7.5
-#comma separated list of platforms. If empty, image will not be multiarch.
-PLATFORMS=linux/amd64 #,linux/arm64
--- Open5Gs-UERANSIM/images/UERANSIM/image_info.sh
+++
@@ -1,3 +0,0 @@
-IMAGE_TAG=3.2.6
-#comma separated list of platforms. If empty, image will not be multiarch.
-PLATFORMS=linux/amd64,linux/arm64
